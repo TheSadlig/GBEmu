6451b96cbafcf21a43b6c9cd8d0095a4ee244864
Gildas Lebel <gildaslebel@hotmail.com>
2022-03-20 21:21:56 -0400

    Add JR action

--- emulator/cpu/commands_actions.py
+++ emulator/cpu/commands_actions.py
@@ -10,3 +10,12 @@ class CommandsActions:
             cpu.register.pc16 = opc.get_param2_value(cpu)
 
         return opc.cycles
+        
+    def JR(cpu:CPU, opc: Opcode) -> int:
+        #   will contain 1 if we have to jump
+        shouldJump = opc.get_param1_value(cpu)
+        if shouldJump == 1:
+            add_to_address = opc.get_param2_value(cpu)
+            cpu.register.pc16 = opc.get_param2_value(cpu) + add_to_address
+
+        return opc.cycles
